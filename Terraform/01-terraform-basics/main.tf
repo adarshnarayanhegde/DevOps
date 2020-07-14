c7198ad3cebc068f0f23bea62b7cdda42499987e
adarshnarayanhegde <adarshnarayanhegde@gmail.com>
2020-07-14 10:54:22 -0400

    divided configurations

--- Terraform/01-terraform-basics/main.tf
+++ Terraform/01-terraform-basics/main.tf
@@ -2,14 +2,3 @@ provider "aws" {
     region = "us-east-1"
     version = "~> 2.70"
 }
-
-resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-adhegde-001"
-    versioning {
-        enabled = true
-    }
-}
-
-resource "aws_iam_user" "my_iam_user" {
-    name = "my-iam-user-a"
-}
